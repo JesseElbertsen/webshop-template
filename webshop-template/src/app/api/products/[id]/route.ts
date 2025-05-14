0202f22d96a3c7b58f940859a7777221e0f9c3d8
Juicy_Jesse <91599008+JesseElbertsen@users.noreply.github.com>
2025-05-14 12:57:04 +0200

    update route.ts

--- webshop-template/src/app/api/products/[id]/route.ts
+++ webshop-template/src/app/api/products/[id]/route.ts
@@ -2,21 +2,26 @@ import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 // Haal één product op
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
   const product = await prisma.product.findUnique({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
   });
   return NextResponse.json(product);
 }
 
 // Werk een product bij
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: { id: string } }
 ) {
-  const data = await req.json();
+  const { id } = context.params;
+  const data = await request.json();
   const product = await prisma.product.update({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
     data,
   });
   return NextResponse.json(product);
@@ -24,11 +29,12 @@ export async function PUT(
 
 // Verwijder een product
 export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: { id: string } }
 ) {
+  const { id } = context.params;
   const product = await prisma.product.delete({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
   });
   return NextResponse.json(product);
 }
